f19a3a1331145f1efaabbe053fc524485fabb662
Luke Camelo <azathothbeats@gmail.com>
2019-02-02 17:10:05 -0500

    Refactored App component to functional useReducer hook based component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,39 +1,73 @@
-import React, { Component } from 'react'
+import React, { useState, useReducer } from 'react'
 import { Card, CardContent, Typography, Input, Button } from '@material-ui/core'
 import api from '../services/api'
 
-class App extends Component {
-  state = {
-    inputValue: '',
-    show: false,
-    results: '',
-    costTotal: 0,
-    itemPrice: 0,
-    profit: 0
-  }
+const initialState = {
+  show: false,
+  results: '',
+  costTotal: 0,
+  itemPrice: 0,
+  profit: 0
+}
 
-  displayItem = name => {
-    api.items.getMargin(name).then(json => {
-      console.log(json.profit)
-      this.setState({
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'show':
+      return {
+        ...state,
         show: true,
-        results: json.item,
-        costTotal: json.materialCost,
-        itemPrice: json.itemPrice,
-        profit: json.profit
-      })
+        results: action.payload.item,
+        costTotal: action.payload.materialCost,
+        itemPrice: action.payload.itemPrice,
+        profit: action.payload.profit
+      }
+    default:
+      return state
+  }
+}
+
+function useInput(initialValue) {
+  const [input, setInput] = useState(initialValue)
+  function handleChange(e) {
+    setInput(e.target.value)
+  }
+  return {
+    value: input,
+    onChange: handleChange
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  let input = useInput('')
+
+  const displayItem = name => {
+    api.items.getMargin(name).then(json => {
+      dispatch({ type: 'show', payload: json })
     })
   }
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  render() {
-    return (
-      <React.Fragment>
+  return (
+    <React.Fragment>
+      <Card style={{ margin: '3rem auto', width: '400px' }}>
+        <CardContent
+          style={{
+            textAlign: 'center',
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <Typography variant="h5">Pick an item!</Typography>
+          <Input name="inputValue" {...input} />
+          <Button
+            style={{ width: '20px', margin: '1rem auto 0 auto' }}
+            onClick={() => displayItem(input.value)}
+          >
+            Submit
+          </Button>
+        </CardContent>
+      </Card>
+      {state.show && (
         <Card style={{ margin: '3rem auto', width: '400px' }}>
           <CardContent
             style={{
@@ -42,47 +76,21 @@ class App extends Component {
               flexDirection: 'column'
             }}
           >
-            <Typography variant="h5">Pick an item!</Typography>
-            <Input
-              name="inputValue"
-              value={this.state.inputValue}
-              onChange={this.handleChange}
-            />
-            <Button
-              style={{ width: '20px', margin: '1rem auto 0 auto' }}
-              onClick={() => this.displayItem(this.state.inputValue)}
-            >
-              Submit
-            </Button>
+            <div>
+              <img src={state.results.item.icon_large} alt="item icon" />
+              <Typography variant="h5">
+                material cost total: {state.costTotal}gp
+              </Typography>
+              <Typography variant="h5">
+                item cost: {state.itemPrice}gp
+              </Typography>
+              <Typography variant="h5">profit: {state.profit}gp</Typography>
+            </div>
           </CardContent>
         </Card>
-        {this.state.show && (
-          <Card style={{ margin: '3rem auto', width: '400px' }}>
-            <CardContent
-              style={{
-                textAlign: 'center',
-                display: 'flex',
-                flexDirection: 'column'
-              }}
-            >
-              <div>
-                <img src={this.state.results.item.icon_large} alt="item icon" />
-                <Typography variant="h5">
-                  material cost total: {this.state.costTotal}gp
-                </Typography>
-                <Typography variant="h5">
-                  item cost: {this.state.itemPrice}gp
-                </Typography>
-                <Typography variant="h5">
-                  profit: {this.state.profit}gp
-                </Typography>
-              </div>
-            </CardContent>
-          </Card>
-        )}
-      </React.Fragment>
-    )
-  }
+      )}
+    </React.Fragment>
+  )
 }
 
 export default App
